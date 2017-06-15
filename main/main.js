889ae70e2c3e67a181ea9461f2bff5e62e69e1dc
ashui <ashui@ashuihouse.cn>
2017-06-15 21:32:38 +0800

    1
    1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,69 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(var0) {
+    var var1=new Array();
+    var var2=new Array();
+    var var3=new Array();
+    var num=0;
+    var relog="";
+    for(var i=0;i<var0.length();i++)
+    {
+        num=parseInt(var0[i]);
+        switch (num)
+        {
+          case 0:
+                var1.push("._.");
+                var2.push("|.|");
+                var3.push("|_|");
+                break;
+          case 1:
+                var1.push("...");
+                var2.push("..|");
+                var3.push("..|");
+                break;
+          case 2:
+                var1.push("._.");
+                var2.push("._|");
+                var3.push("|_.");
+                break;
+          case 3:
+                var1.push("._.");
+                var2.push("._|");
+                var3.push("._|");
+                break;
+          case 4:
+                var1.push("...");
+                var2.push("|_|");
+                var3.push("..|");
+                break;
+          case 5:
+                var1.push("._.");
+                var2.push("|_.");
+                var3.push("._|");
+                break;
+          case 6:
+                var1.push("._.");
+                var2.push("|_.");
+                var3.push("|_|");
+                break;
+          case 7:
+                var1.push("._.");
+                var2.push("..|");
+                var3.push("..|");
+                break;
+          case 8:
+                var1.push("._.");
+                var2.push("|_|");
+                var3.push("|_|");
+                break;
+          case 9:
+                var1.push("._.");
+                var2.push("|_|");
+                var3.push("..|");
+                break;                
+        }
+    }
+    for(var j=0;j<3;j++)
+    {
+        relog=var1.join(" ")+'\n'+var2.join(" ")+'\n'+var3.join(" ")+'\n';
+    }
+    return relog;
 };
